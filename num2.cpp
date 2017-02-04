a64a0393ed567a512881755f9032a63b402ccde4
anonymoussaketjoshi <anonymoussaketjoshi@gmail.com>
2017-02-04 11:11:25 +0530

    No changes

--- num2.cpp
+++ num2.cpp
@@ -4,9 +4,6 @@ using namespace std;
 
 int main(){
     num var2,var;
-    num temp();
-    cin>>temp;'
-    temp+var;
     cin>>var>>var2;
-    cout<<var+var2<<endl<<var-var2<<endl;
+    cout<<var+var2<<endl<<var-var2<<endl<<var*var2<<endl<<var/var2<<endl<<var%var2<<endl;
 }
