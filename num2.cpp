d93bef3d4aee079fcf4488cec2324bb45987281e
anonymoussaketjoshi <anonymoussaketjoshi@gmail.com>
2017-02-04 12:18:13 +0530

    changed

--- num2.cpp
+++ num2.cpp
@@ -5,11 +5,15 @@ using namespace std;
 int main(){
     num var2,var;
     cin>>var>>var2;
-    cout<<(var+var2)<<endl<<var-var2<<endl<<var*var2<<endl<<var/var2<<endl<<var%var2<<endl
-    <<"comparison "<<endl;
+    cout<<"Addition: "<<(var+var2)<<endl
+    <<"Difference: "<<var-var2<<endl
+    <<"Multiply: "<<var*var2<<endl
+    <<"Divide: "<<var/var2<<endl
+    <<"MODULUS: "<<var%var2<<endl;
+/*    <<"comparison "<<endl;
     cout<<((var>var2)?"yes":"no")<<endl
     <<((var>=var2)?"yes":"no")<<endl
     <<((var==var2)?"yes":"no")<<endl
     <<((var<=var2)?"yes":"no")<<endl
-    <<((var<var2)?"yes":"no")<<endl;
+    <<((var<var2)?"yes":"no")<<endl;*/
 }
